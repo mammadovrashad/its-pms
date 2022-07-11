50146ca3ded6a87c7233fad35502902eff14e9b7
Qara <qarahumbtov@gmail.com>
2022-07-11 19:47:34 +0400

    Change App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,7 @@
-import Pages from "./Pages/index"
-
-// From Material UI components
-import {
-  Container
-} from "@mui/material";
-
 const App = () => {
 
   return (
     <div>
-      <Container maxWidth="lg" sx={{ padding: '20px' }}>
-        <Pages />
-      </Container>
     </div>
   )
 }
