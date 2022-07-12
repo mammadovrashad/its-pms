1bcf76e51304c34d3ae491cc559c0aceabe40861
Ulker <ulkrthmzova@gmail.com>
2022-07-12 12:24:21 +0400

    header yaradildi

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import { Fragment } from 'react';
+import Header from './Components/Header';
 import Pages from './Pages'
 const App:React.FC = () => {
   return (
     <Fragment>
+      <Header />
     </Fragment>
   )
 }
--- src/Components/Header/index.tsx
+++ src/Components/Header/index.tsx
@@ -1,7 +1,57 @@
+import './style.css';
+import React from 'react';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import ItsLogo from './img/Asset13';
+import Avatar from './img/Avatar';
+import LogoutIcon from '@mui/icons-material/Logout';
+import { Button, Grid, Menu, MenuItem} from '@mui/material';
+
 const Header:React.FC=()=>{
+    const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+    const open = Boolean(anchorEl);
+    const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+      setAnchorEl(event.currentTarget);
+    };
+    const handleClose = () => {
+      setAnchorEl(null);
+    };
+
     return(
-     <div>
-        
-     </div>
+<section className='container-header'> 
+<Grid container sx={{width:"100%",
+height:'100%',
+padding:'0px 50px'}} alignItems='center' justifyContent='space-between'>
+<Grid item >
+    <ItsLogo />
+</Grid>
+<Grid item>
+<Grid container alignItems='center' spacing={1} >
+<Grid item>
+    <Avatar />
+</Grid>
+<Grid item>
+    <p className='title-style'>Ağarəhim Quluzadə</p>
+</Grid>
+<Grid item>
+<Button
+        onClick={handleClick}
+        sx={{color:'black'}}
+      >
+        <ExpandMoreIcon />
+      </Button>
+      <Menu
+        anchorEl={anchorEl}
+        open={open}
+        onClose={handleClose}
+      >
+        <MenuItem onClick={handleClose}>
+        <LogoutIcon></LogoutIcon>Profildən çıx</MenuItem>
+      </Menu>
+</Grid>
+</Grid>
+</Grid>
+</Grid>
+</section>
     )
 }
+export default Header
