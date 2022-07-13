9a9070e5631a4b7f61b45681727cbafa7903befc
Ulker <ulkrthmzova@gmail.com>
2022-07-13 10:19:37 +0400

    updated header

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,22 @@
-import { Container } from '@mui/material';
 import { Fragment } from 'react';
-/* import AddMemberModal from './Components/Modals/addMemberModal';
+import AddMemberModal from './Components/Modals/addMemberModal';
 import AddPlanModal from './Components/Modals/addPlanModal';
 import DeleteMemberModal from './Components/Modals/deleteMemberModal';
 import DeletePlanModal from './Components/Modals/DeletePlanModal';
 import EditPlanModal from './Components/Modals/editPlanModal';
-import Header from './Components/Header'; */
+import Header from './Components/Header';
 import Pages from './Pages/index'
 const App: React.FC = () => {
   return (
     <Fragment>
-      <Container maxWidth="lg" sx={{ padding: '20px' }}>
-        <Pages />
-      </Container>
-      {/* <Header />
-      <AddMemberModal />
-      <DeleteMemberModal />
-      <AddPlanModal />
-      <DeletePlanModal />
-      <EditPlanModal /> */}
+      <Header />
+    <AddMemberModal />
+     <DeleteMemberModal />
+<AddPlanModal /> 
+     <DeletePlanModal />
+    <EditPlanModal /> 
     </Fragment>
   )
 }
 
-export default App;
+export default App
