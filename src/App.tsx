d5ee85de9153959e5a2b4cac18076012cb20378b
Ulker <ulkrthmzova@gmail.com>
2022-07-14 09:55:59 +0400

    header and modal completed

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,22 @@
 import { Fragment } from 'react';
 import AddMemberTable from './Components/Tables/addMemberTable';
 import CreateTeamTable from './Components/Tables/createTeamTable';
-// import AddMemberModal from './Components/Modals/addMemberModal';
-// import AddPlanModal from './Components/Modals/addPlanModal';
-// import DeleteMemberModal from './Components/Modals/deleteMemberModal';
-// import DeletePlanModal from './Components/Modals/DeletePlanModal';
-// import EditPlanModal from './Components/Modals/editPlanModal';
-// import Header from './Components/Header';
-// import Pages from './Pages/index'
+import AddMemberModal from './Components/Modals/addMemberModal';
+import AddPlanModal from './Components/Modals/addPlanModal';
+import DeleteMemberModal from './Components/Modals/deleteMemberModal';
+import DeletePlanModal from './Components/Modals/DeletePlanModal';
+import EditPlanModal from './Components/Modals/editPlanModal';
+import Header from './Components/Header';
+import Pages from './Pages/index'
 const App: React.FC = () => {
   return (
     <Fragment>
-      {/* <Header />
+      <Header />
     <AddMemberModal />
-     {/* <DeleteMemberModal />
+      <DeleteMemberModal />
 <AddPlanModal /> 
      <DeletePlanModal />
-    <EditPlanModal />  */}
+    <EditPlanModal /> 
     </Fragment>
   )
 }
--- src/Components/Modals/editPlanModal/index.tsx
+++ src/Components/Modals/editPlanModal/index.tsx
@@ -6,6 +6,31 @@ import { ThemeProvider } from '@emotion/react';
 import {  createTheme, Grid } from '@mui/material'
 import './style.css'
 import '../../../Common/Fonts/fonts.css';
+import Textarea from '../../Inputs/Textarea';
+import SelectInput from '../../Inputs/SelectInput';
+import DateInput from '../../Inputs/DateInput';
+import MultilineInput from '../../Inputs/MultilineInput';
+import DisableInput from '../../Inputs/DisableInput';
+import ComboCheckbox  from '../../Inputs/ComboCheckbox';
+import Combobox from '../../Inputs/Combobox';
+const emp = [
+  { id: 1, label: 'Nərmin Ağayeva' },
+  { id: 2, label: 'Nərmin Ağayeva' },
+  { id: 3, label: 'Nərmin Ağayeva' },
+  { id: 4, label: 'Nərmin Ağayeva' },
+  { id: 5, label: 'Nərmin Ağayeva' },
+  { id: 6, label: 'Nərmin Ağayeva' },
+  { id: 7, label: 'Nərmin Ağayeva' },
+  { id: 8, label: 'Nərmin Ağayeva' },
+]
+const stage = [
+  {id: 1, label: 'Analiz'},
+  {id: 2, label: 'Dizayn'},
+  {id: 3, label: 'Development'},
+  {id: 4, label: 'Test'},
+  {id: 5, label: 'Demo'}, 
+  {id: 6, label: 'Tətbiq'}, 
+]
 
   const theme = createTheme({
     typography: {
@@ -32,10 +57,35 @@ const EditPlanModal:React.FC=()=>{
       open={open}
       >
         <Box className='styleplan'>
-    <Grid container>
-        <Grid item xs={12}>
-        <Button onClick ={handleClose}>Ləğv et</Button>
+        <Grid container alignItems='center' sx={{width:'100%', height:'100%'}} >
+      <Grid container justifyContent='space-between' columnSpacing={2}  >
+        <Grid item xs={2.4}><DisableInput labelName='Task nömrəsi' defaultValue='1.2' /></Grid>
+        <Grid item xs={2.4}><Combobox labelName='Mərhələ' placeholder='Mərhələ' position ={stage}/></Grid>
+        <Grid item xs={2.4}><ComboCheckbox  emp={emp}/></Grid>
+        <Grid item xs={2.4}><DateInput labelName='Başlama tarixi' /></Grid>
+        <Grid item xs={2.4}><DateInput labelName='Bitmə tarixi' /></Grid>
+      </Grid>
+      <Grid container justifyContent='space-between' columnSpacing={2}>
+        <Grid item xs={3}><MultilineInput labelName='İcra müddəti' charCount='200' readOnly={false} /></Grid>
+        <Grid item xs={3}><MultilineInput labelName='İcra göstəricisi' charCount='200' readOnly={false} /></Grid>
+        <Grid item xs={3}><DateInput labelName='Son icra tarixi' /></Grid>
+        <Grid item xs={3}><SelectInput /></Grid>
+      </Grid>
+<Grid container justifyContent='space-between' columnSpacing={2}>
+  <Grid item xs={6}><Textarea  labelName='Tapşırığın adı' maxCharCount='2000'></Textarea></Grid>
+  <Grid item xs={6}><Textarea labelName='Tapşırıq üzrə qeydlər' maxCharCount='4000'></Textarea></Grid>
+</Grid>
+      <Grid container justifyContent='space-between'>
+      <Grid item >
+        <Button>Sil</Button>
         </Grid>
+        <Grid item >
+          <div> <Button onClick ={handleClose}>Ləğv et</Button>
+        <Button>Yadda Saxla</Button></div>
+       
+        </Grid>
+        
+      </Grid>
     </Grid>
     </Box>
     </Modal>
